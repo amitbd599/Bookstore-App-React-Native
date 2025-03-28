11b8a4cf061d6a48acb3f85b71e2bc75210d6ec9
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-03-28 23:41:49 +0600

    Update bookRoutes.js

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cloudinary from "../lib/cloudinary.js";
 import middleware from "../middleware/auth.middleware.js";
+import Book from "../models/Book.js";
 
 const router = express.Router();
 
@@ -8,31 +9,53 @@ const router = express.Router();
 router.post("/", middleware, async (req, res) => {
   try {
     let userID = req.userID;
-    // console.log(userID);
+    console.log(userID);
 
-    let { title, caption, img, rating } = req.query;
-    if (!title || !caption || !img || !rating) {
+    let { title, caption, image, rating } = req.body;
+    console.log({ title, caption, image, rating });
+
+    if (!title || !caption || !image || !rating) {
       return res.status(400).json({ message: "Missing required fields" });
     }
 
     // upload the image to cloudinary
-    let uploadResponse = await cloudinary.uploader.upload(img);
+    let uploadResponse = await cloudinary.uploader.upload(image);
     let imgUrl = uploadResponse.secure_url;
     // save the image to the database
 
     // save the details to the database
-    const newImage = await Image.create({
+    const book = await Book.create({
       title,
       caption,
       image: imgUrl,
       rating: parseInt(rating),
-      //   user: req.user.id,
+      user: userID,
     });
 
-    res.status(201).json(newImage);
+    res.status(201).json(book);
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "An error occurred" });
+    console.log(error.toString());
+    res.status(500).json({ message: "An error occurred", error });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    let page = req.params.page || 1;
+    let limit = parseInt(req.query.limit) || 5;
+    let skip = (page - 1) * limit;
+    let books = await Book.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "userName profileImage ");
+
+    let total = await Book.countDocuments();
+    let totalPages = Math.ceil(total / limit);
+    res.status(200).json({ books, currentPage: page, total, totalPages });
+  } catch (error) {
+    console.log(error.toString());
+    res.status(500).json({ message: "An error occurred", error });
   }
 });
 
